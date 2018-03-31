e799e51293e4fdff9223c135724f4e35137048a1
SamatarMumin <SamatarMumin1@gmail.com>
2018-03-31 17:30:45 -0400

    Adding MultiThreaded server with Tehseen and Bevan

--- src/sample/Character.java
+++ src/sample/Character.java
@@ -1,6 +1,12 @@
 package sample;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 
 public class Character {
 
@@ -15,10 +21,21 @@ public class Character {
 
     }
 
-    public void Attack(Character enemy){
+    public void Attack(Character enemy) throws IOException {
         if(this.attack > enemy.defense) {
+          //  String prev_hp = enemy.hp;
+            //Socket other_player = new Socket("");
             enemy.hp = enemy.hp - (this.attack - enemy.defense);
-            //FileWriter write = new FileWriter();
+            FileReader fileReader = new FileReader("character.txt");
+            BufferedReader input = new BufferedReader(fileReader);
+            String line = null;
+            while ((line=input.readLine())!=null){
+                if(line=="hp"){
+                    Scanner scan = new Scanner(fileReader);
+                    String current_hp=scan.next();
+                }
+
+            }
             //todo file io to the char info file
         }
         else{//todo add output to UI for dmg dealt
